dfa110ebc10dafe14a4a2d8990f4f5963c45f15e
EsraKaranfil <esrakaranfileb@gmail.com>
2024-05-09 23:59:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+import MainPage from './Components/MainPage/MainPage';
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <MainPage/>
+      
+
+    </div>
+  );
+}
+
+export default App;
